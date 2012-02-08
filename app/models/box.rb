78c0f7701e3f3870299ba495d18e035dbe4e26e8
Troy Murray <troy.murray@gmail.com>
2012-02-08 10:45:11 -0500

    Add validation

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,4 +1,13 @@
 class Box < ActiveRecord::Base
 	belongs_to :order
 	has_one :widget
+
+	#validates_numericality_of :size
+
+		# Validations
+	validates :color,
+											:presence => true,
+											:uniqueness => true
+
+	
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -2,4 +2,15 @@ class Widget < ActiveRecord::Base
 	belongs_to :box
 	has_many :categorizations
 	has_many :categories, :through => :categorizations
+
+	# Validations
+	validates :name,
+											:presence => true,
+											:length => { :minimum => 3},
+											:uniqueness => true
+
+	validates :color,
+											:inclusion => %w(red white blue RED WHITE BLUE Red White Blue)
+
+
 end
